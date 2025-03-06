fc49775072712cf9eab05b494868a4d82b8cd6ca
MOHAMMED FAHEEM MADHIA <131679679+Fahu783@users.noreply.github.com>
2025-03-06 20:24:11 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+
+int main() {
+    char s[100];
+    scanf("%[^\n]", s); 
+
+    printf("Hello, World!\n");
+    printf("%s", s); 
+
+    return 0;
+}
+
